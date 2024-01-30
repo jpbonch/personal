9fd00105c9755231e2acc77cd8a4555da0c48a03
Joao Pedro Amaral Bonchristiano <jp.bonchristiano@gmail.com>
2024-01-30 00:57:58 -0500

    init

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto({ weight: ['100', '300', '400', '500', '700'], subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} style={{backgroundColor: "#eeeeee"}}>{children}</body>
     </html>
   );
 }
